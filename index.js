5d7b1103ad4ffb51212ed85423e9bb3432c8a913
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-04-11 19:17:43 +0530

    Moving to Typescript

--- index.js
+++
@@ -1,6 +0,0 @@
-
-import { NativeModules } from 'react-native';
-
-const { RNReactNativeBackgroundService } = NativeModules;
-
-export default RNReactNativeBackgroundService;
---
+++ index.ts
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { ReactNativeBackgroundService } = NativeModules;
+
+export default ReactNativeBackgroundService;
